cef01d194b06b35845c1a0f46a6cd02692f1bfbe
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-07-02 12:31:59 +0530

    Report a bug here

--- resources/views/follow-req.blade.php
+++ resources/views/follow-req.blade.php
@@ -1,10 +1,11 @@
-<p style="font-size:18px;text-align:center;color:#FF5722;">This project is still under development and has issues.<br>Stay tuned on twitter to get updates for stable release.</p>
+<p style="font-size:18px;text-align:center;color:#FF5722;">This project is still under development and has issues. Stay tuned on twitter to get updates for stable release.</p>
 <center>
 <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">LaraAdmin - Laravel CRUD Generator with FormMaker and Artisan Command. Takes 5 minutes for up and running. <a href="https://t.co/yy39EHRDAH">https://t.co/yy39EHRDAH</a> <a href="https://twitter.com/hashtag/laravel?src=hash">#laravel</a></p>&mdash; LaraAdmn (@laraadmn) <a href="https://twitter.com/laraadmn/status/748079473371848704">June 29, 2016</a></blockquote>
 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 
 <p style="font-size:16px;text-align:center;color:#FF5722;margin-top:10px;">
-	<span style="vertical-align:super;">Or star the Github repo:</span> 
+	<span style="vertical-align:super;margin-right:5px;">Or star the Github repo:</span> 
 	<a class="github-button" href="https://github.com/dwijitsolutions/laraadmin" data-icon="octicon-star" data-count-href="/dwijitsolutions/laraadmin/stargazers" data-count-api="/repos/dwijitsolutions/laraadmin#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star dwijitsolutions/laraadmin on GitHub">Star</a>
+	<a style="color:#FF5722;vertical-align:super;margin-left:5px;font-weight:bold;" href="https://github.com/dwijitsolutions/laraadmin/issues">Report a bug here</a>
 </p>
 </center>
